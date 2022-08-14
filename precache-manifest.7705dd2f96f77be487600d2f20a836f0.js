79d27b847e82a487581a98186fbbf645fce232a5
Your Name <abishek@inapp.com>
2022-08-15 03:04:58 +0530

    Updates

---
+++ precache-manifest.7705dd2f96f77be487600d2f20a836f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214e1219af007782ed428628cad94108",
+    "url": "/Wedding/index.html"
+  },
+  {
+    "revision": "ce9a827bfdc7fb0e731b",
+    "url": "/Wedding/static/css/main.3fbf0bbf.chunk.css"
+  },
+  {
+    "revision": "ffa8c2485477aa6f3272",
+    "url": "/Wedding/static/js/2.4f866eee.chunk.js"
+  },
+  {
+    "revision": "3b20e8403fac097246e191f3dfea4c90",
+    "url": "/Wedding/static/js/2.4f866eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9a827bfdc7fb0e731b",
+    "url": "/Wedding/static/js/main.b490670d.chunk.js"
+  },
+  {
+    "revision": "2dede22a19c8b107421f",
+    "url": "/Wedding/static/js/runtime-main.3475a1b4.js"
+  },
+  {
+    "revision": "fbe8db0777d53d41dfa8afa01e69348e",
+    "url": "/Wedding/static/media/backgroundImage.fbe8db07.png"
+  }
+]);
